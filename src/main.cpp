a76048db5dfa4ed4810a4295bc8ba3ea2eb2b6ba
harha <toni.s.h@netikka.fi>
2016-11-06 01:34:31 +0200

    More HTTP-related stuff. HappyHttp was not a good choice, I have to find a replacement.

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,11 @@
 #include <winsock2.h>
 #endif // _WIN32 
 
+// For convenience
 using json = nlohmann::json;
 using namespace gateway;
 
+// Gateway data
 static UA_StatusCode gateway_status;
 static std::vector<OPCUA_Client *> gateway_clients;
 static json gateway_settings;
@@ -24,19 +26,17 @@ static json gateway_database_servers;
 
 static UA_SubscriptionSettings gateway_subscription_settings =
 {
-	// requestedPublishingInterval
-	100.0,
-	// requestedLifetimeCount
-	10000,
-	// requestedMaxKeepAliveCount
-	1,
-	// maxNotificationsPerPublish
-	10,
-	// publishingEnabled
-	true,
-	// priority
-	1
+	100.0,	// requestedPublishingInterval
+	10000,	// requestedLifetimeCount
+	1,		// requestedMaxKeepAliveCount
+	10,		// maxNotificationsPerPublish
+	true,	// publishingEnabled
+	1		// priority
 };
+
+// HTTP Content-Type's
+const std::string ct_urlencoded = "application/x-www-form-urlencoded";
+const std::string ct_json = "application/json";
 
 // Callback: HTTP Request begin
 void HTTPOnBegin(const happyhttp::Response * r, void * userdata)
@@ -48,6 +48,10 @@ void HTTPOnBegin(const happyhttp::Response * r, void * userdata)
 void HTTPOnData(const happyhttp::Response * r, void * userdata, const unsigned char * data, int n)
 {
 	LOG("HTTP Request data, Response status: %d, n: %d\n", UA_DateTime_now(), r->getstatus(), n);
+
+	// Return if no data
+	if (n <= 2)
+		return;
 
 	// Get request type
 	char * req_type = static_cast<char *>(userdata);
@@ -62,7 +66,7 @@ void HTTPOnData(const happyhttp::Response * r, void * userdata, const unsigned c
 		// Parse the JSON data and store it to memory
 		gateway_database_servers = json::parse(data_string);
 
-		LOG("HTTP Request GET /opcuaservers, json: %s\n", UA_DateTime_now(), gateway_database_servers.dump());
+		LOG("HTTP Request GET /opcuaservers, json:\n\n%s\n\n", UA_DateTime_now(), gateway_database_servers.dump());
 	} break;
 	}
 }
@@ -85,7 +89,7 @@ int main(int argc, char * argv[])
 		ERR("WSAStartup failed. Error code: %d\n", UA_DateTime_now(), code);
 		return 0;
 	}
-#endif // _WIN32 
+#endif // _WIN32
 
 	// Read settings in JSON format
 	std::ifstream settings("./res/settings.json", std::ifstream::binary);
@@ -138,7 +142,7 @@ int main(int argc, char * argv[])
 			std::string endpoint = gateway_settings["ua_servers_config"][i]["endpoint"].get<std::string>();
 			std::string username = gateway_settings["ua_servers_config"][i]["username"].get<std::string>();
 			std::string password = gateway_settings["ua_servers_config"][i]["password"].get<std::string>();
-			int32_t serverId = gateway_settings["ua_servers_config"][i]["id"].get<int32_t>();
+			int32_t serverId = gateway_settings["ua_servers_config"][i]["serverId"].get<int32_t>();
 
 			// Create client instance
 			OPCUA_Client * client = new OPCUA_Client(endpoint, username, password, serverId);
@@ -169,13 +173,54 @@ int main(int argc, char * argv[])
 				}
 			}
 
+			// Get number of servers in database
+			size_t n_db_servers = gateway_database_servers.size();
+
+			// Does this server exist in database?
+			bool exists_in_db = false;
+			for (size_t j = 0; j < n_db_servers; j++)
+			{
+				int32_t db_serverId = gateway_database_servers[j]["serverId"].get<int32_t>();
+
+				if (serverId == db_serverId)
+				{
+					exists_in_db = true;
+					break;
+				}
+			}
+
+			// Insert / Update the server entry in database
+			if (exists_in_db == false)
+			{
+				// Convert data to proper format
+				std::string data_string = gateway_settings["ua_servers_config"][i].dump();
+				const unsigned char * data = (const unsigned char *)data_string.c_str();
+
+				// Create the POST request
+				try
+				{
+					conn.setcallbacks(HTTPOnBegin, HTTPOnData, HTTPOnComplete, static_cast<void *>("POST /opcuaservers"));
+					conn.putrequest("POST", "/opcuaservers");
+					conn.putheader("Connection", "close");
+					conn.putheader("Content-Length", data_string.size());
+					conn.putheader("Content-Type", ct_json.c_str());
+					conn.putheader("Accept", "text/plain");
+					conn.endheaders();
+					conn.send(data, data_string.size());
+				}
+				catch (const happyhttp::Wobbly & w)
+				{
+					ERR("Wobbly: %s\n", UA_DateTime_now(), w.what());
+				}
+			}
+
 			// Push the client into clients vector
 			gateway_clients.push_back(client);
 		}
 	}
 	catch (const std::exception & e)
 	{
-		ERR("%s\n", UA_DateTime_now(), e.what());
+		ERR("Excpetion: %s\n", UA_DateTime_now(), e.what());
 	}
 
 	// Main loop, exit if an error occurs
@@ -187,6 +232,12 @@ int main(int argc, char * argv[])
 			c->update();
 
 			gateway_status = c->getStatus();
+		}
+
+		// Update HTTP requests
+		if (conn.outstanding())
+		{
+			conn.pump();
 		}
 
 		// Sleep for a bit, just to keep the OS sane
